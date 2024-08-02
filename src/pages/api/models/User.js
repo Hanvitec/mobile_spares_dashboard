b4d8580a3867761c9b8909a4b657d41ff778d9ed
Pratheek <saipratheekreddykasarla@gmail.com>
2024-08-02 18:48:36 +0530

    Modified user schema (for validaition purpose).

--- src/pages/api/models/User.js
+++ src/pages/api/models/User.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-// Define schema for delivery address
 const addressSchema = new Schema({
   _id: Schema.Types.ObjectId,
   street: { type: String, required: true },
@@ -12,30 +11,17 @@ const addressSchema = new Schema({
   country: { type: String, required: true }
 });
 
-// Define schema for order items with delivery address
-const orderItemSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
-  quantity: { type: Number, required: true },
-  price: { type: Number, required: true },
-  deliveryAddress: { type: addressSchema, required: true }
-});
-
-// Define schema for cart items
 const cartItemSchema = new Schema({
   _id: Schema.Types.ObjectId,
   productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
   quantity: { type: Number, required: true }
 });
 
-// Define schema for wishlist items
 const wishlistItemSchema = new Schema({
   _id: Schema.Types.ObjectId,
-  productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
-  // categoryId: { type: Schema.Types.ObjectId, ref: 'Category', required: true }
+  productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true }
 });
 
-// Define schema for user details
 const userDetailsSchema = new Schema({
   businessName: { type: String },
   name: { type: String },
@@ -44,14 +30,15 @@ const userDetailsSchema = new Schema({
   permanentAddress: { type: addressSchema, required: true }
 }, { _id: false });
 
-// User Schema
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   type: { type: String, enum: ['Business', 'Individual'], required: true },
   userDetails: { type: userDetailsSchema, required: true },
-  orderHistory: [orderItemSchema],
+  images: [{ type: String }], // Multiple image URLs
+  isValidated: { type: Boolean, default: false }, // Validation field
+  orderHistory: [{ type: Schema.Types.ObjectId, ref: 'Order' }], // Reference to Order schema
   cart: [cartItemSchema],
   wishlist: [wishlistItemSchema],
   deliveryAddress: [addressSchema]
